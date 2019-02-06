fd2f311ca77eccba4d7d9c785ba352296176bf37
didiloki <eiweala@live.com>
2019-02-06 13:40:02 +0300

    commandline

---
+++ warmups/06-warmups.js
@@ -0,0 +1,58 @@
+/*
+    Current Date 
+
+    Write a function that displays the current date and time in this format(Wednesday 06 Feb 2019, 06:00:00 PM)
+
+    Suggestions
+
+  Use JS built-in Date class
+
+    Date - 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+*/
+
+var currentDate = function(){
+
+
+}
+
+
+/*
+    Time converter
+
+    Write a program that, takes a number in seconds and converts it into time in Hours and mins
+
+    Suggestions
+
+    Use JS built-in Date class
+
+    Date - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+   
+
+*/
+
+var secToHourMins = function(num){
+
+}
+
+
+/*
+Anagram Detector
+
+Write a program that, given a word and a list of possible anagrams, selects the anagrams of the given word.
+
+In other words, given: "listen" and ["enlists" "google" "inlets" "banana" "istlen"] the program should return inlets and istlen.
+
+Suggestions
+
+Use JS built-in functions to your advantage
+split() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+sort()- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/sort
+join()- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/join
+
+*/
+
+var anamgramDetector = function(str){
+
+}
